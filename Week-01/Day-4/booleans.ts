c08818c77a034914ff48f4bae73cf84f0fce9da5
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 14:42:32 +0100

    Booleans

---
+++ Week-01/Day-4/booleans.ts
@@ -0,0 +1,22 @@
+console.log(true);
+console.log (false);
+console.log(!true);
+console.log(!false);
+
+console.log(true&&true);
+console.log (true&&false);
+console.log(false&&true);
+console.log(false&&false);
+
+console.log(true||true);
+console.log (true||false);
+console.log(false||true);
+console.log(false||false);
+
+console.log('apple');
+console.log('dont\'t');
+console.log('"Everything you can imagine is real" - Picasso');
+
+console.log("uni"+"corn");
+
+console.log('My favorite numbr is: '+ 7);
