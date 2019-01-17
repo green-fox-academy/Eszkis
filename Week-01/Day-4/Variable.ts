531a7e03756f897cb24a502bf7ba5b4c9e71f6ec
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 15:02:48 +0100

    Variable

---
+++ Week-01/Day-4/Variable.ts
@@ -0,0 +1,21 @@
+let welcome:string='Hello World!';
+console.log(welcome);
+
+let isAwsome:boolean=true;
+console.log(isAwsome);
+
+let theMeaningOfLifeAndTheUniverseAndEverything:number=42;
+console.log(theMeaningOfLifeAndTheUniverseAndEverything);
+
+let pi:number=3.141592;
+console.log(pi);
+
+let number:number=12;
+number=23;
+
+let dynamic:any=5;
+console.log(dynamic);
+dynamic=true;
+console.log(dynamic);
+dynamic='strange';
+console.log(dynamic);
