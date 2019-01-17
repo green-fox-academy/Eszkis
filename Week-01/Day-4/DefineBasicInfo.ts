1cf74051087f821c9727bece76851a1029714d4e
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 15:29:33 +0100

    DefneBasicInfo

---
+++ Week-01/Day-4/DefineBasicInfo.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+let myName: string = "Sz. Kis László";
+let age: number = 26;
+let myHeight: number = 1.94;
+let married: boolean = false;
+
+console.log(myName);
+console.log(age);
+console.log(myHeight);
+console.log(married);
