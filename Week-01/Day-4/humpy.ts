f6c38a85fb90edb7eed936a540bf0c2639a95b39
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 14:13:52 +0100

    humpy

---
+++ Week-01/Day-4/humpy.ts
@@ -0,0 +1,6 @@
+'use strict';
+
+console.log('Humpty Dumpty sat on a wall,');
+console.log('Humpty Dumpty had a great fall.');
+console.log('All the king\'s horses and all the king\'s men');
+console.log('Couldn\'t put Humpty together again.');
