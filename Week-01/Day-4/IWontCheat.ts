b59fc5d9f3221784aabf87b1bc73d3b1b721f0df
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 16:31:41 +0100

    IWontCheat

---
+++ Week-01/Day-4/IWontCheat.ts
@@ -0,0 +1,8 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let i:number=0;
+for(i=0;i<=100;i++)
+console.log("I won't cheat on the exam!");
