a0cbb04a3bc1c484e4c15ce760caa607221244be
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 16:27:34 +0100

    loops

---
+++ Week-01/Day-4/Loopsts.ts
@@ -0,0 +1,9 @@
+let a: number = 0;
+while (a < 10) {
+  console.log(a); // Prints the numbers from 0 to 9
+  a++;
+}
+
+for (let i: number = 0; i < 100; i++) {
+  console.log(i); // Prints the numbers from 0 to 99
+}
