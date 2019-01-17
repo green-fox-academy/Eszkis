d2b1643e2009da98a0f5d26a53f4e276896e14b9
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 15:08:30 +0100

    Arithmatic

---
+++ Week-01/Day-4/Arithmtic.ts
@@ -0,0 +1,37 @@
+let a:number=12;
+a=a+4;
+console.log(a);
+
+let b:number=12;
+b=b-4
+console.log(b);
+
+let c:number=12;
+c*=3;
+console.log(c);
+
+let d:number=12;
+d /=3;
+console.log(d);
+
+let e:number=12;
+e%=7;
+console.log(e);
+
+let f:number=12;
+f++;
+console.log(f);
+
+let g:number=12;
+g--;
+console.log(g);
+
+let h:number=10;
+console.log(h);
+console.log(h++);
+console.log(h);
+
+let i:number=10;
+console.log(i);
+console.log(++i);
+console.log(i);
