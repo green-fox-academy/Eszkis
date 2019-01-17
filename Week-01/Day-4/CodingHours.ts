dea8dabfb9be96bec0b248ed6749f01572314eb2
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 14:55:20 +0100

    CodingHours

---
+++ Week-01/Day-4/CodingHours.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+console.log('Attandee codes '+ 17*5*6 +' hours in a semester.');
+
+console.log('Attandee codes ' + (((17*5*6)/(52*17))*100).toFixed(2) + '% of his/hers working hours during the semester.');
