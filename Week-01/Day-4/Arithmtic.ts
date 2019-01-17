931ec2753cbdc671040012a554f1633dd9dea4a5
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 15:14:29 +0100

    FavoiteNumber

--- Week-01/Day-4/Arithmtic.ts
+++ Week-01/Day-4/Arithmtic.ts
@@ -1,37 +1,37 @@
-let a:number=12;
-a=a+4;
+let a: number = 12;
+a = a + 4;
 console.log(a);
 
-let b:number=12;
-b=b-4
+let b: number = 12;
+b = b - 4
 console.log(b);
 
-let c:number=12;
-c*=3;
+let c: number = 12;
+c *= 3;
 console.log(c);
 
-let d:number=12;
-d /=3;
+let d: number = 12;
+d /= 3;
 console.log(d);
 
-let e:number=12;
-e%=7;
+let e: number = 12;
+e %= 7;
 console.log(e);
 
-let f:number=12;
+let f: number = 12;
 f++;
 console.log(f);
 
-let g:number=12;
+let g: number = 12;
 g--;
 console.log(g);
 
-let h:number=10;
+let h: number = 10;
 console.log(h);
 console.log(h++);
 console.log(h);
 
-let i:number=10;
+let i: number = 10;
 console.log(i);
 console.log(++i);
 console.log(i);
---
+++ Week-01/Day-4/FavoriteNumber.ts
@@ -0,0 +1,5 @@
+'use strict';
+
+let favoriteNumber:number=8;
+
+console.log("My favorite number is "+favoriteNumber+ "!");
