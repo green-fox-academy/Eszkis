dc897e3577151683076e09236572aa037dfde2d1
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-17 16:34:20 +0100

    PrintEvent

---
+++ Week-01/Day-4/PrintEven.ts
@@ -0,0 +1,10 @@
+'use strict';
+
+// Create a program that writes this line 100 times:
+// "I won't cheat on the exam!"
+
+let i:number=0;
+for(i=0;i<=500;i++)
+    if(i%2===0){
+        console.log(i);
+    }
