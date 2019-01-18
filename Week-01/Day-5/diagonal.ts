d6481357cf4d872399db0dbc20d22c3f171273e5
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-18 11:06:09 +0100

    diagonal

---
+++ Week-01/Day-5/diagonal.ts
@@ -0,0 +1,45 @@
+'use strict';
+
+let lineCount: number = 6;
+let i: number = 0;
+let x: number = 0;
+let y: number = 0;
+let line: string = "";
+
+// Write a program that draws a square like this:
+//
+// %%%%%%
+// %%   %
+// % %  %
+// %  % %
+// %   %%
+// %%%%%%
+//
+// The square should have as many lines as lineCount is
+
+for (i = 1; i <= lineCount; i++) {
+    if (i === 1) {
+        for (x = 1; x <= lineCount; x++) {
+            line = line + "%";
+        }
+        console.log(line);
+        line = "";
+    } else if (i === lineCount) {
+        for (x = 1; x <= lineCount; x++) {
+            line = line + "%";
+        }
+        console.log(line);
+        line = "";
+    } else {
+        line = "%"
+        for (y = 1; y <= (lineCount - 2); y++) {
+            if((y+1)===i){
+                line = line + "%";
+            }else
+            line = line + " ";
+        }
+        line = line + "%";
+        console.log(line);
+        line = ""
+    }
+}
