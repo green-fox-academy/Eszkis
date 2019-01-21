f0f1a48211c331c9baa3270b4f3e9fc24ca22e33
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-21 15:40:00 +0100

    DoubleItems

---
+++ Week-02/Day-1/DoubleItemstxt.ts
@@ -0,0 +1,12 @@
+// -  Create an array variable named `drinks` with the following content: `['Gin', 'Whiskey', 'Wine', 'Beer']`
+// -  Double all the strings in the array, use a built in array method instead of a loop
+// It should print: ['GinGin', 'WhiskeyWhiskey', 'WineWine', 'BeerBeer']`
+
+
+let drinks: string[] = ['Gin', 'Whiskey', 'Wine', 'Beer']
+
+let drinks2 = drinks.map(function (double) {
+  return (double = double + double);
+})
+
+console.log(drinks2);
