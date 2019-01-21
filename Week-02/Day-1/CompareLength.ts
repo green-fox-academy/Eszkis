f753cfce374a478fea3214b81775ae664b50efb6
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-21 14:26:08 +0100

    CompareLength

---
+++ Week-02/Day-1/CompareLength.ts
@@ -0,0 +1,15 @@
+// -  Create a variable named `firstList`
+//    with the following content: `[1, 2, 3]`
+// -  Create a variable named `secondList`
+//    with the following content: `[4, 5]`
+// -  Log to the console if `secondList` has more elements than `firstList`
+
+let firstList: number[] = [1, 2, 3];
+let secondList: number[] = [4, 5];
+
+if (firstList.length > secondList.length) {
+  console.log('First list has more elements');
+
+} else {
+  console.log('Second list has more elements');
+}
