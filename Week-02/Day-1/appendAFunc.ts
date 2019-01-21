32dcefebecf18044f47f8ded43312aa13a8400ec
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-21 13:45:06 +0100

    appendAFunc

---
+++ Week-02/Day-1/appendAFunc.ts
@@ -0,0 +1,16 @@
+// -  Create a string variable named `typo` and assign the value `Chinchill` to it
+// -  Write a function called `appendAFunc` that gets a string as an input,
+//    appends an 'a' character to its end and returns with a string
+// -  Print the result of `appendAFunc(typo)`
+
+let typo:string='Chinchill';
+
+function appendAFunc(x:string) {
+      if(x===undefined){
+        return 'a'
+      }else{
+        return x+"a"
+      }
+}
+
+console.log(appendAFunc(typo));
