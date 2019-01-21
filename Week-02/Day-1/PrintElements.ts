f26b02828b0e7b56f6a9b379c38a76904bf00b78
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-21 14:49:25 +0100

    printElements

---
+++ Week-02/Day-1/PrintElements.ts
@@ -0,0 +1,10 @@
+// -  Create a variable named `integers` with the following content: `[4, 5, 6, 7]`
+// -  Log each the element of `integers` to the console*
+// *hint: use a loop, console.log(integers) won't cut it
+// -  bonus for using the correct built in array method
+
+let integers:number[]=[4,5,6,7];
+
+integers.forEach(function(integers){
+  console.log(integers);});
+  
