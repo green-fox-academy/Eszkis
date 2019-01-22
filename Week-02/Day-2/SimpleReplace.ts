7445d6df19a0992eaf70286d77509a74f5c44e48
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-22 13:28:38 +0100

    replace

---
+++ Week-02/Day-2/SimpleReplace.ts
@@ -0,0 +1,11 @@
+'use strict';
+
+// I would like to replace "dishwasher" with "galaxy" in this example
+// Please fix it for me!
+// Expected ouput: In a galaxy far far away
+
+let example: string = 'In a dishwasher far far away';
+
+example=example.replace('dishwasher','galaxy');
+
+console.log(example);
