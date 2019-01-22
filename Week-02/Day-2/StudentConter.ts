23e8801d1b7068f575be383c4be1ddbd15f44f53
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-22 19:21:40 +0100

    SudentConter

---
+++ Week-02/Day-2/StudentConter.ts
@@ -0,0 +1,17 @@
+'use strict';
+
+const students: any[] = [
+  {name: 'Theodor', age: 3, candies: 2},
+  {name: 'Paul', age: 9.5, candies: 2},
+  {name: 'Mark', age: 12, candies: 5},
+  {name: 'Peter', age: 7, candies: 3},
+  {name: 'Olaf', age: 12, candies: 7},
+  {name: 'George', age: 10, candies: 1}
+];
+
+// create a function that takes a list of students and logs: 
+// - How many candies are owned by students altogether
+
+// create a function that takes a list of students and logs:
+// - The sum of the age of people who have less than 5 candies
+
