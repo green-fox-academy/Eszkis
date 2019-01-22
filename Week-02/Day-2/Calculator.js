208aa6e0de734134df1a7f017d2c06cade8ab858
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-22 17:14:58 +0100

    Calculator

---
+++ Week-02/Day-2/Calculator.js
@@ -0,0 +1,52 @@
+'use strict';
+// Create a simple calculator application which does read the parameters from the standard input
+// and prints the result to the console.
+
+// It should support the following operations, create function called "calculate()" :
+// +, -, *, /, % and it should support two operands:
+
+// The format of the expressions must be: {operation} {operand} {operand}.
+// Examples: "+ 3 3" (the result will be 6) or "* 4 4" (the result will be 16)
+
+// You should use the command line arguments to accept user input
+
+// It should work like this:
+
+// Start the program with "node calculator.js + 5 6"
+// If number of arguments are not correct, print some nice errors
+// Else print the result
+// Say goodbye
+
+// Just a helper for you to get started
+let operat = process.argv[2];
+let numb1 = parseInt(process.argv[3]);
+let numb2 = parseInt(process.argv[4]);
+
+if ((numb1 !== numb1 || numb2 !== numb2) && operat !== '+' && operat !== '-' && operat !== '\*' && operat !== '\u002F' && operat !== '%') {
+  console.log('One of arguments are invalid. Please start the program again, and give right arguments')
+} else {
+  switch (operat) {
+    case '+':
+      console.log('Result: ' + (numb1 + numb2));
+      console.log('Goodbye');
+      break;
+    case '-':
+      console.log('Result: ' + (numb1 - numb2));
+      console.log('Goodbye');
+      break;
+    case '\*':
+      console.log('Result: ' + (numb1 * numb2));
+      console.log('Goodbye');
+      break;
+    case '\u002F':
+      console.log('Result: ' + (numb1 / numb2));
+      console.log('Goodbye');
+      break;
+    case '%':
+      console.log('Result: ' + (numb1 % numb2));
+      console.log('Goodbye');
+      break;
+    default:
+  }
+}
+
