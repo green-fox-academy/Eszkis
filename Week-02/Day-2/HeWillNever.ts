a0a29d2b01088aada1d198816d1ea563652b2b85
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-22 14:08:09 +0100

    HeWillNever

---
+++ Week-02/Day-2/HeWillNever.ts
@@ -0,0 +1,24 @@
+'use strict';
+// Things are a little bit messed up
+// Your job is to decode the notSoCrypticMessage by using the hashmap as a look up table
+// Assemble the fragments into the out variable
+
+let out: string = '';
+let notSoCrypticMessage: number[] = [1, 12, 1, 2, 11, 1, 7, 11, 1, 49, 1, 3, 11, 1, 50, 11];
+
+let hashmap = {
+  7: 'run around and desert you',
+  50: 'tell a lie and hurt you ',
+  49: 'make you cry, ',
+  2: 'let you down',
+  12: 'give you up, ',
+  1: 'Never gonna ',
+  11: '\n',
+  3: 'say goodbye '
+};
+
+for (let x: number = 0; x < notSoCrypticMessage.length; x++) {
+  out += hashmap[notSoCrypticMessage[x]];
+}
+
+console.log(out)
