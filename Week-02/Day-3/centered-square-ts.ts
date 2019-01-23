c8a8692b6d85b0ec10ff2d8daa4721b15275f3af
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-23 09:29:42 +0100

    CenteredSquare

---
+++ Week-02/Day-3/centered-square-ts.ts
@@ -0,0 +1,11 @@
+
+const canvas = document.querySelector('.main-canvas') as HTMLCanvasElement;
+const ctx = canvas.getContext('2d');
+
+// DO NOT TOUCH THE CODE ABOVE THIS LINE
+
+// Draw a green 10x10 square to the center of the canvas.
+
+
+ctx.strokeStyle='blue';
+ctx.fillRect(250,150,100,100);
