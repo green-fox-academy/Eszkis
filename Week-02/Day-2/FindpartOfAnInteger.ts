784c3c20977956b7c68e0f9721a8cfd0f624b81e
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-23 08:50:45 +0100

    line-in-the-middle

---
+++ Week-02/Day-2/FindpartOfAnInteger.ts
@@ -0,0 +1,13 @@
+'use strict';
+//  Create a function that takes a number and a list of numbers as a parameter
+//  Returns the indeces of the numbers in the list where the first number is part of
+//  Returns an empty list if the number is not part any of the numbers in the list
+
+// Example
+//console.log(subint(1, [1, 11, 34, 52, 61]));
+// should print: `[0, 1, 4]`
+//console.log(subint(9, [1, 11, 34, 52, 61]));
+// should print: '[]'
+
+let findNumb:number=1;
+let numbArray:number[]=[4,23,42,53,123,52,12];
