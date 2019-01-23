57d6fcd990a89c21d5a05454c60d18244692ffce
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-23 14:19:23 +0100

    FindPartOfAnInteger

--- Week-02/Day-2/FindpartOfAnInteger.ts
+++ Week-02/Day-2/FindpartOfAnInteger.ts
@@ -4,10 +4,20 @@
 //  Returns an empty list if the number is not part any of the numbers in the list
 
 // Example
-//console.log(subint(1, [1, 11, 34, 52, 61]));
-// should print: `[0, 1, 4]`
 //console.log(subint(9, [1, 11, 34, 52, 61]));
 // should print: '[]'
 
-let findNumb:number=1;
-let numbArray:number[]=[4,23,42,53,123,52,12];
+function subint(findNumb: number, numbArray: number[]) {
+  let stringArray: number[] = numbArray.filter(actualNumb => {
+    let actualString: string = actualNumb.toString();
+    if (actualString.indexOf(findNumb.toString()) >= 0) {
+      return actualString;
+    }
+  });
+  return stringArray.map(num => numbArray.indexOf(num));
+}
+
+console.log(subint(1, [1, 11, 34, 52, 61]));
+// should print: `[0, 1, 4]`
+console.log(subint(9, [1, 11, 34, 52, 61]));
+// should print: '[]'
