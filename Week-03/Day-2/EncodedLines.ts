a908c64fd2b17f0ceaab64c4f190a5eaf36b5f71
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-29 23:23:11 +0100

    EncodedLines

---
+++ Week-03/Day-2/EncodedLines.ts
@@ -0,0 +1,24 @@
+'use strict';
+
+// Create a method that decrypts encoded-lines.txt
+
+let fs = require('fs');
+
+function reversedLines(fileName: string) {
+  let content: string[] = fs.readFileSync(fileName, 'utf-8').split('\r');
+  let array: string[][] = [];
+  let sentences: string = '';
+  content.forEach(function (value) {
+    array.push(value.split(''))
+  })
+  for (let x: number = 0; x < array.length; x++) {
+    for (let y: number = 0; y < array[x].length; y++) {
+      if (array[x][y] === '\n' || array[x][y] === ' ') {
+        sentences += array[x][y]
+      }
+      sentences += String.fromCharCode(array[x][y].charCodeAt(0) - 1)
+    }
+  }
+  console.log(sentences);
+}
+reversedLines('EncodedLines.txt')
