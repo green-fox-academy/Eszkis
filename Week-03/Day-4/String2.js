fb24962c0282caf46e4e7caf9e79c9160d3100a5
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-31 11:35:06 +0100

    String2

---
+++ Week-03/Day-4/String2.js
@@ -0,0 +1,13 @@
+// Given a string, compute recursively a new string where all the 'x' chars have been removed.
+
+function switchX(string) {
+  if (string === '') {
+    return ''
+  } else if (string.substr(0, 1) === 'x'||string.substr(0, 1) === 'X') {
+    return ('' + switchX(string.substr(1)));
+  } else {
+    return string.substr(0, 1) + switchX(string.substr(1))
+  }
+}
+
+console.log(switchX('XxXxXxXasdv'));
