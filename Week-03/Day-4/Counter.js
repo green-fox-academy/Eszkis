91631820d2a198e227868b4f7b3122de939f7a51
Sz. Kis László <laszlo.szkis@gmail.com>
2019-01-31 09:31:46 +0100

    Counter_v2&NumberAdder

--- Week-03/Day-4/Counter.js
+++ Week-03/Day-4/Counter.js
@@ -1,11 +1,12 @@
 // Write a recursive function that takes one parameter: n and counts down from n.
 
 function counter(startNumber) {
-  if (startNumber <=1) {
-    return 1
+  if (startNumber <= 1) {
+    console.log(1);
   } else {
-    return startNumber += counter(startNumber - 1)
+    console.log(startNumber);
+    startNumber = counter(startNumber - 1)
   }
 }
 
-console.log(counter(4));
+counter(1)
