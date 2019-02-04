1a9f198990144dac2e7c7ed703c14abc5fe2a35f
Sz. Kis László <laszlo.szkis@gmail.com>
2019-02-04 12:37:44 +0100

    Pirates_v2

--- Week-03/Day-3/pirates.ts
+++ Week-03/Day-3/pirates.ts
@@ -79,7 +79,7 @@ class Pirate {
 
 class Ship {
   crew: Pirate[];
-  private score: number;
+  score: number;
   private captian: Pirate;
   private aliveCrew: number;
 
@@ -106,6 +106,7 @@ class Ship {
   }
 
   calculateScore() {
+    this.aliveCheck();
     this.score = this.aliveCrew - this.captian.getInfo().rumNumber
   }
 
@@ -138,3 +139,81 @@ class Ship {
   }
 }
 
+class BattleApp {
+
+  ship1: Ship;
+  ship2: Ship;
+
+  constructor(ship1Input: Ship, ship2Input: Ship) {
+    this.ship1 = ship1Input;
+    this.ship2 = ship2Input;
+    this.ship1.fillShip(5);
+    this.ship2.fillShip(6);
+  }
+
+  startBattle() {
+    this.ship1.battle(this.ship2)
+  }
+}
+
+class Armada {
+  ships: Ship[] = []
+
+  constructor() {
+    this.ships = []
+  }
+
+  fillArmada(numberOfShips: number, numberOfCrew: number) {
+    for (let index: number = 0; index < numberOfShips; index++) {
+      this.ships.push(new Ship);
+      this.ships[index].fillShip(numberOfCrew);
+      this.ships[index].calculateScore();
+    }
+  }
+
+  war(Armada: Armada) {
+    for (let armada1Ready: number = 0; armada1Ready < this.ships.length; armada1Ready++) {
+      this.ships[armada1Ready].calculateScore();
+    }
+
+    for (let armada2Ready: number = 0; armada2Ready < Armada.ships.length; armada2Ready++) {
+      Armada.ships[armada2Ready].calculateScore();
+    }
+
+    let armada1ShipNumber: number = 0;
+    let armada2ShipNumber: number = 0;
+    let winner: boolean = true;
+
+    while (armada1ShipNumber < this.ships.length && armada2ShipNumber < Armada.ships.length) {
+      if (this.ships[armada1ShipNumber].score > Armada.ships[armada2ShipNumber].score) {
+        ++armada2ShipNumber;
+        winner = true;
+      } else {
+        ++armada1ShipNumber;
+        winner = false;
+      }
+    }
+    return winner;
+  }
+}
+
+class WarApp {
+  armada1: Armada;
+  armada2: Armada;
+
+  constructor(armada1Input: Armada, armada2Input: Armada) {
+    this.armada1 = armada1Input;
+    this.armada2 = armada2Input;
+  }
+
+  war() {
+    return this.armada1.war(this.armada2)
+  }
+}
+
+let armada1 = new Armada;
+let armada2 = new Armada;
+armada1.fillArmada(3, 6);
+armada2.fillArmada(3, 5);
+let WarApp1 = new WarApp(armada1, armada2);
+console.log(WarApp1.war());
