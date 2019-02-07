134f344bedb9ac715ab26a1d42ab51b18c82cbe0
Sz. Kis László <laszlo.szkis@gmail.com>
2019-02-07 14:48:34 +0100

    Extensions with checking, most of them wrong

--- Week-04/Day-4/ExtensionTest.ts
+++ Week-04/Day-4/ExtensionTest.ts
@@ -4,33 +4,33 @@ let test = require('tape')
 
 import { add, maxOfThree, median, isVowel, translate } from './extension';
 
-test('add: 2 and 3 is 5', function (t: any): any {
-  t.equal(add(2, 3), 5);
+test('add: 2 and 5 is 7', function (t: any): any {
+  t.equal(add(2, 5), 7);
   t.end();
 });
 
-test('add: 1 and 4 is 5', function (t: any): any {
-  t.equal(add(1, 4), 5);
+test('add: 1 and 3 is 4', function (t: any): any {
+  t.equal(add(1, 3), 4);
   t.end();
 });
 
 test('max of three: first', function (t: any): any {
-  t.equal(maxOfThree(5, 4, 3), 5);
+  t.equal(maxOfThree(3, 84, 15), 84);
   t.end();
 });
 
 test('max of three: third', function (t: any): any {
-  t.equal(maxOfThree(3, 4, 5), 5);
+  t.equal(maxOfThree(8, 45, -3), 45);
   t.end();
 });
 
 test('median: four', function (t: any): any {
-  t.equal(median([7, 5, 3, 5]), 5);
+  t.equal(median([4, 3, 0, 1]), 2);
   t.end();
 });
 
 test('median: five', function (t: any): any {
-  t.equal(median([1, 2, 3, 4, 5]), 3);
+  t.equal(median([16, 14, 0, 8, 6]), 8.8);
   t.end();
 });
 
@@ -44,17 +44,17 @@ test('is vowel: u', function (t: any): any {
   t.end();
 });
 
-test("is vowel: k", function(t: any): any {
+test("is vowel: k", function (t: any): any {
   t.notOk(isVowel("k"));
   t.end();
 });
 
-test('translate: bemutatkozik', function (t: any): any {
-  t.equal(translate('bemutatkozik'), 'bevemuvutavatkovozivik');
+test('translate: integet', function (t: any): any {
+  t.equal(translate('integet'), 'vave');
   t.end();
 });
 
-test('translate: lagopus', function (t: any): any {
-  t.equal(translate('lagopus'), 'lavagovopuvus');
+test('translate: zsír', function (t: any): any {
+  t.equal(translate('szír'), 'fat');
   t.end();
 });
