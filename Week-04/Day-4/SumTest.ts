8705dbc4c5df2407cab54d04461eecb102f63bd0
Sz. Kis László <laszlo.szkis@gmail.com>
2019-02-07 12:06:15 +0100

    Sum Empty array, and one number=>works

--- Week-04/Day-4/SumTest.ts
+++ Week-04/Day-4/SumTest.ts
@@ -4,11 +4,7 @@ let test = require('tape')
 
 test('Sum a list of number', t => {
   const listOfNumbers = new Sum;
-  listOfNumbers.addNumber(2);
-  listOfNumbers.addNumber(4);
   listOfNumbers.addNumber(5);
-  listOfNumbers.addNumber(8);
-
-  t.equal(listOfNumbers.sumNumber(), 19);
+  t.equal(listOfNumbers.sumNumber(), 5);
   t.end();
 });
