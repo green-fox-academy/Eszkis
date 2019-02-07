da7631cb4778912102fc35760424f91cc3a24212
Sz. Kis László <laszlo.szkis@gmail.com>
2019-02-07 14:23:33 +0100

    Fibonacci test with not number input

--- Week-04/Day-4/Fibonacci.ts
+++ Week-04/Day-4/Fibonacci.ts
@@ -1,11 +1,15 @@
 'use strict'
 
 export function fibonacci(n) {
-  if (n === 0) {
-    return 0;
-  } else if (n === 1) {
-    return 1;
+  if (typeof (n) === 'number') {
+    if (n === 0) {
+      return 0;
+    } else if (n === 1) {
+      return 1;
+    } else {
+      return fibonacci(n - 1) + fibonacci(n - 2)
+    }
   } else {
-    return fibonacci(n - 1) + fibonacci(n - 2)
+    return `${n} is not a number`
   }
 }
--- Week-04/Day-4/FibonacciTest.ts
+++ Week-04/Day-4/FibonacciTest.ts
@@ -3,16 +3,16 @@ import { fibonacci } from './Fibonacci';
 let test = require('tape')
 
 test('counts all the letters', t => {
-  t.deepEqual(fibonacci(5),5 );
+  t.deepEqual(fibonacci(5), 5);
   t.end();
 });
 
 test('counts all the letters', t => {
-  t.deepEqual(fibonacci(1),1 );
+  t.deepEqual(fibonacci(1), 1);
   t.end();
 });
-/* 
+
 test('counts all the letters', t => {
-  t.deepEqual(fibonacci('a'),1 );
+  t.deepEqual(fibonacci('a'), 'a is not a number');
   t.end();
-}); */
+});
