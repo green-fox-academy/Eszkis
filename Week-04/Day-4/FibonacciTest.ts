4b81462826aa3068a62771e1ef6a644b45602dcf
Sz. Kis László <laszlo.szkis@gmail.com>
2019-02-07 13:58:57 +0100

    Fibonacci test with number

--- Week-04/Day-4/FibonacciTest.ts
+++ Week-04/Day-4/FibonacciTest.ts
@@ -3,6 +3,16 @@ import { fibonacci } from './Fibonacci';
 let test = require('tape')
 
 test('counts all the letters', t => {
-  t.deepEqual(fibonacci(5), );
+  t.deepEqual(fibonacci(5),5 );
   t.end();
 });
+
+test('counts all the letters', t => {
+  t.deepEqual(fibonacci(1),1 );
+  t.end();
+});
+/* 
+test('counts all the letters', t => {
+  t.deepEqual(fibonacci('a'),1 );
+  t.end();
+}); */
