574506ee0d530d2f4d2045795d2a109d77d1f6e2
Sz. Kis László <laszlo.szkis@gmail.com>
2019-02-07 13:56:13 +0100

    Fibonacci with out test

--- Week-04/Day-4/CountLettersTest.ts
+++ Week-04/Day-4/CountLettersTest.ts
@@ -6,3 +6,8 @@ test('counts all the letters', t => {
   t.deepEqual(CountLetters('asdsdd'), [['a', 1], ['d', 3], ['s', 2]]);
   t.end();
 });
+
+test('counts all the letters', t => {
+  t.deepEqual(CountLetters('asdsdd0'), [['a', 1], ['d', 3], ['s', 2]]);
+  t.end();
+});
---
+++ Week-04/Day-4/FibonacciTest.ts
@@ -0,0 +1,8 @@
+import { fibonacci } from './Fibonacci';
+
+let test = require('tape')
+
+test('counts all the letters', t => {
+  t.deepEqual(fibonacci(5), );
+  t.end();
+});
