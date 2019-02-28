062e82fdbd3e102b9729ac3f9a10c434b588745e
Sz. Kis László <laszlo.szkis@gmail.com>
2019-02-28 14:44:53 +0100

    Greetings

---
+++ Week-06/Day-4/Greetings/index.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.set('view engine', 'ejs');
+
+app.get('/', (reg, res) => {
+  res.render('home', { query: reg.query });
+});
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
+})
