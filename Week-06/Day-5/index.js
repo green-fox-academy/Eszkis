765f999eecde91a5a3ea567ffcaa9cc04586ce45
Sz. Kis László <laszlo.szkis@gmail.com>
2019-03-01 15:04:21 +0100

    some tweek

--- Week-06/Day-5/index.js
+++ Week-06/Day-5/index.js
@@ -8,7 +8,7 @@ const images = [
   { name: 'War1', description: 'First horseman you can play', img: 'static\\images\\War1.jpg' },
   { name: 'War2', description: 'He is the one who found out, that the angels broke the final seal', img: 'static\\images\\War2.jpg' },
   { name: 'Death1', description: 'The one who rides the pale horse. Main caracter in the second game.', img: 'static\\images\\Death1.jpg' },
-  { name: 'Death2', description: 'Do anything to save his brother war. Even sacrifice his own, and fellow nephalems life for him. ', img: 'static\\images\\Death2.jpg' },
+  { name: 'Death2', description: 'Do anything to save his brother War. Even sacrifice his, and fellow nephalems life for him. ', img: 'static\\images\\Death2.jpg' },
   { name: 'Fury1', description: 'The most egoist from the four. Only live for the thrill of the fights.', img: 'static\\images\\Fury1.jpg' },
   { name: 'Fury2', description: 'She saves a lot of the remaining humans from earth during her quest.', img: 'static\\images\\Fury2.jpg' },
 ];
