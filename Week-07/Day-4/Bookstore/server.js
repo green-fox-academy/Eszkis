801a296513f6431e9f1baef8a54f8a11da69ec81
Sz. Kis László <laszlo.szkis@gmail.com>
2019-03-07 11:59:36 +0100

    set up env

--- Week-07/Day-4/Bookstore/server.js
+++ Week-07/Day-4/Bookstore/server.js
@@ -3,7 +3,36 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const mysql = require('mysql');
+const path = require('path');
+
+app.set('view engine', 'ejs');
+
+app.use('/static', express.static('static'));
+
+const conn = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+});
 
 app.listen(port, () => {
   console.log(`Server is running at port: ${port}`);
 })
+
+app.get('/', (req, res) => {
+  // res.render('home')
+  conn.query('SHOW TABLES')
+});
+
+app.get('/books', (req, res) => {
+  conn.query('SELECT book_name FROM book_mast;', (err, rows) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send();
+      return;
+    }
+    res.send(rows);
+  });
+});
