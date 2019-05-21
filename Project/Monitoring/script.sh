7a0e50aa3f69f72c42b9184858f460201893efe7
Eszkis <laszlo.szkis@gmail.com>
2019-05-21 15:33:17 +0200

    started working on  "Pulumi" user stroy's documentation

--- Project/Monitoring/script.sh
+++ Project/Monitoring/script.sh
@@ -7,6 +7,6 @@ sudo usermod -aG docker ec2-user
 sudo service docker start
 sudo sysctl -w vm.max_map_count=262144
 sudo docker pull docker.elastic.co/elasticsearch/elasticsearch:7.1.0
-sudo docker run -d -p 9200:9200 -p 9300:9300 -it -h elasticsearch --name elasticsearch docker.elastic.co/elasticsearch/elasticsearch:7.1.0
+sudo docker run -d -p 9200:9200 -p 9300:9300 -it -m 2g -h elasticsearch --name elasticsearch docker.elastic.co/elasticsearch/elasticsearch:7.1.0
 #sudo docker pull docker.elastic.co/kibana/kibana:7.0.1
 #sudo docker run -p 5601:5601 -it --name kibana --link elasticsearch:elasticsearch docker.elastic.co/kibana/kibana:7.0.1
