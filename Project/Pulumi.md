51398121b7b38524017ae464d2cc082540021884
Eszkis <laszlo.szkis@gmail.com>
2019-05-22 14:22:42 +0200

    finished documentation for pulumi, Hello Word web app working

--- Project/Pulumi.md
+++ Project/Pulumi.md
@@ -1,2 +1,55 @@
 # Pulumi
 
+## Prerequisites
+
+ - AWS:
+  - awscli installed
+  - AWS credentials configured
+ - Pulumi installed
+ - Pulumi access token
+ - Terraform installed
+ - WebServer application
+
+## Setting up
+
+Open an empty folder in terminal and give the following command:
+
+
+```
+pulumi new aws-javascript
+```
+
+
+This will pull the dependencies for a new javascript project to be deployed on AWS, and creates a pulumi project. You will need to configure some setting for your aws instance.
+
+
+
+To run your script and deploy, enter this command:
+
+
+```
+pulumi up#
+#
+```
+
+
+This command will also create a stack.
+You can destroy the stack with this command:
+
+
+```
+pulumi stack rm
+```
+
+
+To undo everything your script did enter this command:
+
+
+```
+pulumi destroy
+```
+
+
+Note: as of now, pulumi doesn't support provisioners. Use user data to run scripts on your instances.
+
+[useful tool to convert Terraform files to JS and other languages](https://github.com/pulumi/tf2pulumi)
--- Project/Pulumi/index.js
+++ Project/Pulumi/index.js
@@ -1,38 +1,29 @@
 "use strict"
 const aws = require("@pulumi/aws");
 
-let size = "t2.micro";     // t2.micro is available in the AWS free tier
-let ami  = "ami-0ebe657bc328d4e82"; // AMI for Amazon Linux in us-east-1 (Virginia)
-let userData=`#cloud-boothook
-#!/bin/bashv
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.32.0/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 4.4.5
-sudo yum install -y git
-git clone https://github.com/green-fox-academy/malachite_ops_adam_ec2.git
-cd malachite_ops_adam_ec2
-npm install
-npm install pm2 -g
-echo start
-pm2 start hello.js
-`;
+const size = "t2.micro";
+const ami  = "ami-0ebe657bc328d4e82";
+let userData =`#cloud-boothook
+#!/bin/bash
+sudo echo "Hello, World!" >> index.html
+sudo nohup python -m SimpleHTTPServer 80 &`;
 
-let group = new aws.ec2.SecurityGroup("ec2-allow-ssh", { 
-    ingress: [
-        { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
-        { protocol: "tcp", fromPort: 9005, toPort: 9005, cidrBlocks: ["0.0.0.0/0"] },
-    ],
-    egress:[
-      { fromPort: 22, toPort: 22, protocol: "tcp", cidrBlocks: ["0.0.0.0/0"] },
-      { fromPort: 9005, toPort: 9005, protocol: "tcp", cidrBlocks: ["0.0.0.0/0"] },
-    ],
+let group = new aws.ec2.SecurityGroup("webserver-secgrp", {
+  ingress: [
+    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] }
+  ],
+  egress: [
+    { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
+    { protocol: "tcp", fromPort: 80, toPort: 80, cidrBlocks: ["0.0.0.0/0"] }
+  ]
 });
 
 let server = new aws.ec2.Instance("webserver-www", {
     instanceType: size,
-    securityGroups: [ group.name ], // reference the security group resource above
     ami: ami,
     tags: {"Name":"HelloWorld"},
+    securityGroups: [group.name],
     userData: userData,
     keyName: "megalotis-devops"
 });
