a737b934cdb5b63167f7f57d42f2ccb91e0da8c7
Eszkis <laszlo.szkis@gmail.com>
2019-08-21 20:03:28 +0200

    finished week2 day1

--- Java/Week2/Day1/Bubble.java
+++ Java/Week2/Day1/Bubble.java
@@ -7,15 +7,47 @@ public class Bubble {
     // Example:
     System.out.println(bubble(new int[] { 34, 12, 24, 9, 5 }));
     // should print [5, 9, 12, 24, 34]
-    System.out.println(advancedBubble(new int[] { 34, 12, 24, 9, 5 }, true));
+     System.out.println(advancedBubble(new int[] { 34, 12, 24, 9, 5 }, true));
     // should print [34, 24, 12, 9, 5]
   }
 
   public static String bubble(int[] input) {
-    String order = "";
+    String order = "[";
+    int temp = 0;
     for (int i = 0; i < input.length; i++) {
-      
+      for (int j = 1; j < input.length - i; j++) {
+        if (input[j - 1] > input[j]) {
+          temp = input[j - 1];
+          input[j - 1] = input[j];
+          input[j] = temp;
+        }
+      }
     }
+    for (int number : input) {
+      order += String.valueOf(number) + ", ";
+    }
+    order = order.substring(0, order.length() - 2);
+    order += "]";
+    return order;
+  }
+
+  public static String advancedBubble(int[] input, Boolean sortOrder) {
+    String order = "[";
+    int temp = 0;
+    for (int i = 0; i < input.length; i++) {
+      for (int j = 1; j < input.length - i; j++) {
+        if (input[j - 1] < input[j]) {
+          temp = input[j - 1];
+          input[j - 1] = input[j];
+          input[j] = temp;
+        }
+      }
+    }
+    for (int number : input) {
+      order += String.valueOf(number) + ", ";
+    }
+    order = order.substring(0, order.length() - 2);
+    order += "]";
     return order;
   }
 }
